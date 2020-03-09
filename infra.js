acc4f9df412e4fb5d8823b956830b3361270f383
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-03-09 23:43:14 +0400

    fix

--- infra.js
+++ infra.js
@@ -1,4 +1,7 @@
-Event.handler('Controller.onshow', function () {
+Event.handler('Controller.onshow', async () => {
+	let Load = (await import('/vendor/akiyatkin/load/Load.js')).default
+	let CDN = await Load.on('import-default', '/vendor/akiyatkin/load/CDN.js')
+	await CDN.load('jquery')
 	var city = City.get();
 	if (Env.get().city == city) return;	
 	$('.-city-str').html(city);
