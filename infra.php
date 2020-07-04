1d76f8188a16aac61d2944c76414f644e2679013
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-07-04 05:38:59 +0400

    fix

--- infra.php
+++ infra.php
@@ -5,6 +5,7 @@ use infrajs\lang\Lang;
 use infrajs\ip\IP;
 use infrajs\config\Config;
 use infrajs\event\Events;
+use infrajs\db\Db;
 use akiyatkin\city\City;
 
 Env::add('city', function ($data) {
@@ -14,6 +15,11 @@ Env::add('city', function ($data) {
 	return $get['ru'];
 }, function ($city) {
 	$conf = Config::get('city');
-	if (empty($conf['list'])) return true;
-	return in_array($city, $conf['list']);
+	if ($conf['list']) return in_array($city, $conf['list']);
+
+	$stmt = Db::stmt('SELECT city from cities where city = ?');
+	$stmt->execute(array($city));
+	$r = $stmt->fetchColumn();
+	if ($r) return true; 
+	
 });
