bb7d2f4afb16f1fd63cff1666c96b697ee54bf24
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-10-21 09:18:05 +0400

    fix

--- update.php
+++ update.php
@@ -4,5 +4,5 @@
 use infrajs\load\Load;
 use infrajs\ans\Ans;
 
-$ans = Load::loadJSON('-city/api/update-sql?submit=1');
-if (!$ans['result']) die($ans['msg']);
+//$ans = Load::loadJSON('-city/api/update-sql?submit=1');
+//if (!$ans['result']) die($ans['msg']);
